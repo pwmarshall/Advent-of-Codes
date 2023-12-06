abc53cd3686a1b1f1adf018694ada6f31810c41e
killerkitten82 <pwmarshall@gmail.com>
2023-12-06 00:14:38 -0600

    no dumb dumnb

--- AOC 2023/Day 6/Day 6 oneline.py
+++ AOC 2023/Day 6/Day 6 oneline.py
@@ -1,3 +1,5 @@
 time, distance = [int(i.split(" ")[1]) for i in open("input2.txt", "r").read().split("\n")]
 holds = int((time + (time**2-4*distance)**0.5)/2) - int((time - (time**2-4*distance)**0.5)/2)
 print(holds)
+
+print(int((44806572 + (44806572**2-4*208158110501102)**0.5)/2) - int((44806572 - (44806572**2-4*208158110501102)**0.5)/2))
