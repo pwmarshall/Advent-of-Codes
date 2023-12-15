1a5f0feeca78e9bbe6203bdd13b5f76c56c6f884
killerkitten82 <pwmarshall@gmail.com>
2023-12-15 00:11:31 -0600

    Day 15

--- AOC 2023/Day 14/Day 14 pt2.py
+++ AOC 2023/Day 14/Day 14 pt2.py
@@ -26,30 +26,58 @@ def rollRow(row):
     # print(returnRow)
     return returnRow
 
-for i in range(4):
-    rocks = [[rocks[i][j] for i in range(len(rocks))] for j in range(len(rocks[0]))]
+def rollRowReversed(row:list):
+    # print(row)
+    returnRow = []*len(row)
+    if row.count("#") == 0:
+        rollCount = row.count("O")
+        returnRow[:(len(row)-rollCount)] = "." * (len(row)-rollCount)
+        returnRow[(len(row)-rollCount):] = "O"*rollCount
+    else:
+        indexRock = row.index("#")
+        returnRow = rollRowReversed(row[:indexRock])
+        returnRow.append('#') 
+        returnRow.extend(rollRowReversed(row[indexRock+1:]))
 
-    # backRocks = [[rocks[i][j] for i in range(len(rocks))] for j in range(len(rocks[0]))]
-    print()
-    for line in rocks:
-        print("".join(line))
+    # print(returnRow)
+    return returnRow
 
-    for i in range(len(rocks)):
-        rocks[i] = rollRow(rocks[i])
+cycles = 1000000000
+percent = cycles / 100
+for j in range(cycles): # cycles
+    for i in range(4): #4 directions
+        rocks = [[rocks[i][j] for i in range(len(rocks))] for j in range(len(rocks[0]))]
 
-    print()
-    for line in rocks:
-        print("".join(line))
+        # backRocks = [[rocks[i][j] for i in range(len(rocks))] for j in range(len(rocks[0]))]
+        # print()
+        # print(i)
+        # for line in rocks:
+        #     print("".join(line))
+
+        if i == 0 or i == 1:
+            for i in range(len(rocks)):
+                rocks[i] = rollRow(rocks[i])
+        else:
+            for i in range(len(rocks)):
+                rocks[i] = rollRowReversed(rocks[i])
+
+        # print()
+        # for line in rocks:
+        #     print("".join(line))
+    
+    # print()
+    # for line in rocks:
+    #     print("".join(line))
+    # if (i / cycles) * 100 
+    if i % percent == 0:
+        print(f"{(i / cycles) * 100}% done")
 
 # for line in colRocks:
 #     print("".join(line))
 
 print(sum([sum([len(rocks[i]) - j for j in range(len(rocks[i])) if rocks[i][j] == "O"]) for i in range(len(rocks))]))
 
-## transpose back bc i bad
-
-backRocks = [[rocks[i][j] for i in range(len(rocks))] for j in range(len(rocks[0]))]
 
 print()
-for line in backRocks:
+for line in rocks:
     print("".join(line))
---
+++ AOC 2023/Day 15/Day 15.py
@@ -0,0 +1,72 @@
+f = open("input.txt", "r")
+strings = f.read().split(",")
+print(strings)
+
+
+# Determine the ASCII code for the current character of the string.
+# Increase the current value by the ASCII code you just determined.
+# Set the current value to itself multiplied by 17.
+# Set the current value to the remainder of dividing itself by 256.
+
+
+def hash(string):
+    hashValue = 0
+    for character in string:
+        hashValue += ord(character)
+        hashValue *= 17
+        hashValue %= 256
+
+    return hashValue
+
+print(sum([hash(i) for i in strings]))
+
+
+table = {}
+for i in range(256):
+    table[i] = []
+
+intructions = [i.split("=") if "=" in i else i.split("-") for i in strings]
+print(intructions)
+
+for label, focal in intructions:
+    h = hash(label)
+    # print(label)
+    labels = [table[h][i][0] for i in range(len(table[h]))]
+    # print(labels)
+
+    if focal == "":
+        if label in labels:
+            table[h].pop(labels.index(label))
+    else:
+        if label in labels:
+            table[h][labels.index(label)][1] = focal
+        else:
+            table[h].append([label, focal])
+
+
+    # if len(label) == 1:
+    #     #removing
+    #     if label[0] in table[h]:
+    #         table[h].remove(label[0])
+    # elif len(label) == 2:
+    #     # adding
+    #     if label[0] in table[h]:
+    #         table[h][table[h].index(label[0])] = label[1]
+    #     else:
+    #         table[h].append(label[1])
+    # else:
+    #     print("no")
+            
+print(table)
+
+sumFocusingPower = 0
+for i in range(256):
+    # print(i, "i")
+    for j in range(len(table[i])):
+        # print(j, "j")
+        focusingPower = (i+1) * (j+1) * int(table[i][j][1])
+        # print(focusingPower, "power")
+        sumFocusingPower += focusingPower
+        
+
+print(sumFocusingPower)
